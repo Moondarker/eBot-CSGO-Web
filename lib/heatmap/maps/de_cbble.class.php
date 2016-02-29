c577eb4da43dc7b486a6167c1d44237541b92211
Peter Pyatkin <vaker007full@gmail.com>
2016-03-01 01:17:01 +0300

    Fixed conflicts after merging

--- lib/heatmap/maps/de_cbble.class.php
+++ lib/heatmap/maps/de_cbble.class.php
@@ -1,24 +1,6 @@
 <?php
 
 class de_cbble extends MapsHeatmap {
-
-<<<<<<< HEAD
-    public function __construct($match_id) {
-        $this->setStartX(-3488);
-        $this->setStartY(-2068);
-        $this->setEndX(887);
-        $this->setEndY(3039);
-        $this->setFlipV(true);
-        $this->setResX(500);
-        $this->setResY(585);
-        $this->calcSize();
-        $this->setMatchId($match_id);
-    }
-
-    public function getMapImage() {
-        return "/images/maps/csgo/overview/de_cbble.png";
-    }
-=======
         public function __construct($match_id) {
                 $this->setStartX(-3496);
                 $this->setStartY(-2072);
@@ -34,8 +16,6 @@ class de_cbble extends MapsHeatmap {
         public function getMapImage() {
                 return "/images/maps/csgo/overview/de_cbble.png";
         }
->>>>>>> refs/remotes/deStrO/master
-
 }
 
 ?>
